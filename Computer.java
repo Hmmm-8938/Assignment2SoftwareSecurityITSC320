30736c24524e50d87a068b7d24bd4d4750b61b3d
RomerJohn <romerjohn.delina@edu.sait.ca>
2025-02-27 13:52:06 -0700

    Inheritance to Composition
    Changed the computer.java class from inheritance to composition. The program will not work unless the other classes are changed as well.

--- Computer.java
+++ Computer.java
@@ -1,33 +1,33 @@
-//Computer class: manages computer CPU, RAM and Disk information
-
+// Computer class: manages computer CPU, RAM, and Disk information
 public class Computer {
-    String CPU=null;
-    String RAM=null;
-    String disk=null;
+    private String CPU;
+    private String RAM;
+    private String disk;
 
-    //Constructors
-    public Computer() {} //No-arg contructor
+    // Constructors
+    public Computer() {
+    } // No-arg constructor
 
     public Computer(String CPU, String RAM, String disk) {
-        this.CPU=CPU;
-        this.RAM=RAM;
-        this.disk=disk;
+        this.CPU = CPU;
+        this.RAM = RAM;
+        this.disk = disk;
     }
 
-    //Setters
+    // Setters
     public void setCPU(String CPU) {
-        this.CPU=CPU;
+        this.CPU = CPU;
     }
 
     public void setRAM(String RAM) {
-        this.RAM=RAM;
+        this.RAM = RAM;
     }
 
     public void setDisk(String disk) {
-        this.disk=disk;
+        this.disk = disk;
     }
 
-    //Getters
+    // Getters
     public String getCPU() {
         return this.CPU;
     }
@@ -40,5 +40,8 @@ public class Computer {
         return this.disk;
     }
 
-
-}
+    @Override
+    public String toString() {
+        return "CPU: " + CPU + ", RAM: " + RAM + ", Disk: " + disk;
+    }
+} // Computer class
