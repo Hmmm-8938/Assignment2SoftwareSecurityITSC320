41034373f69faa92bfc40bfa905a2407b91314d6
Colin Ly <152212714+eugeney1@users.noreply.github.com>
2025-02-27 22:24:55 -0700

    desktop composition

--- Desktop.java
+++ Desktop.java
@@ -1,34 +1,27 @@
-//Desktop computer: adds GPU type
+public class Desktop {
+    private Computer computer; 
+    private String GPUType;
 
-public class Desktop extends Computer { //Inherits from Computer
-    String GPUType=null;
-
-    //Constructors
-    public Desktop() {} //No-arg constructor
-
+    // Constructors
     public Desktop(String CPU, String RAM, String disk, String GPUType) {
-        //Inherited from Computer superclass
-        this.CPU=CPU;
-        this.RAM=RAM;
-        this.disk=disk;
-
-        //Only in Desktop subclass
-        this.GPUType=GPUType;
+        this.computer = new Computer(CPU, RAM, disk); // Delegate to Computer
+        this.GPUType = GPUType;
     }
 
-    //Setter
+    public Computer getComputer() {
+        return computer;
+    }
+
     public void setGPUType(String GPUType) {
-        this.GPUType=GPUType;
+        this.GPUType = GPUType;
     }
-
-    //Getter
+    
     public String getGPUType() {
         return this.GPUType;
     }
-
-    //Return formatted version of data
+    
+    @Override
     public String toString() {
-        return "Type:Desktop\tCPU:" + this.CPU + "\tRAM:" + this.RAM + "\tDisk:" + this.disk + "\tGPU:" + this.GPUType;
+        return "Type:Desktop\t" + computer.toString() + "\tGPU:" + this.GPUType;
     }
-
 }
