41c1f01341deaa79ecd3fd1fc0316916c640babe
Hmmm8938 <39926063+Hmmm-8938@users.noreply.github.com>
2025-03-06 22:11:22 -0700

    Added single list
    Added an interface of type Computable which will allow all devices to be of a Computable type allowing them to be within the same arrayList

--- Desktop.java
+++ Desktop.java
@@ -1,28 +1,54 @@
 //changed to composition 
 
-public class Desktop {
+public class Desktop implements Computable 
+{
+    private String CPU;
+    private String RAM;
+    private String disk;
     private Computer computer; 
     private String GPUType;
 
-    public Desktop(String CPU, String RAM, String disk, String GPUType) {
+    // Constructors
+    public Desktop() 
+    {
+    
+    } // No-arg contructor
+
+    public Desktop(String CPU, String RAM, String disk, String GPUType) 
+    {
         this.computer = new Computer(CPU, RAM, disk); 
         this.GPUType = GPUType;
     }
 
-    public Computer getComputer() {
-        return computer;
+    // Getters
+    public String getCPU() 
+    {
+        return this.CPU;
     }
 
-    public void setGPUType(String GPUType) {
-        this.GPUType = GPUType;
+    public String getRAM() 
+    {
+        return this.RAM;
+    }
+
+    public String getDisk() 
+    {
+        return this.disk;
+    }
+
+    public Computer getComputer() 
+    {
+        return computer;
     }
     
-    public String getGPUType() {
+    public String getGPUType() 
+    {
         return this.GPUType;
     }
     
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Type:Desktop\t" + computer.toString() + "\tGPU:" + this.GPUType;
     }
 }
--- Laptop.java
+++ Laptop.java
@@ -1,27 +1,53 @@
 //Laptop computer: adds screen size to other Computer info
-public class Laptop {
+public class Laptop implements Computable 
+{
+    private String CPU;
+    private String RAM;
+    private String disk;
     private Computer computer;
     private String screenSize;
 
-    public Laptop(String CPU, String RAM, String disk, String screenSize) {
+    // Constructors
+    public Laptop() 
+    {
+        
+    } // No-arg contructor
+
+    public Laptop(String CPU, String RAM, String disk, String screenSize) 
+    {
         this.computer = new Computer(CPU, RAM, disk); // Delegate to Computer
         this.screenSize = screenSize;
     }
 
-    public Computer getComputer() {
+    // Getters
+    public String getCPU() 
+    {
+        return this.CPU;
+    }
+    
+    public String getRAM() 
+    {
+        return this.RAM;
+    }
+    
+    public String getDisk() 
+    {
+        return this.disk;
+    }
+
+    public Computer getComputer() 
+    {
         return computer;
     }
 
-    public void setScreenSize(String screenSize) {
-        this.screenSize = screenSize;
-    }
-
-    public String getScreenSize() {
+    public String getScreenSize() 
+    {
         return this.screenSize;
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Type:Laptop\t" + computer.toString() + "\tScreen:" + this.screenSize;
     }
 }
